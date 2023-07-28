14f578faaf56c53fe7bc66b701410d574a3e6cf2
Padmini8 <Micky@098>
2023-07-28 11:25:11 +0530

    first commit

---
+++ Test1_Selenium/Login.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('http://dosmartqa.stagsoftware.net:3009/login')
+
+driver.find_element(By.NAME,"email").send_keys("Padmini")
+time.sleep(4)
+driver.find_element(By.ID,"password").send_keys("Micky@098")
+time.sleep(4)
+driver.find_element(By.CLASS_NAME,"form-submit-button").click()
+time.sleep(4)
+driver.find_element(By.ID,"projectselectExistOpen").click()
+time.sleep(4)
+driver.find_element(By.XPATH,"//input[@type='checkbox']").click()
+time.sleep(4)
